ebe2c402437be6891ee00eb48b742dcf3d352310
imaginechan2467 <imaginechan2467@gmail.com>
2022-09-10 11:33:42 +0900

    [add]UniTask追加

--- EL_2022_09_10_2D/Assets/Program/Scripts/SC_MG_Game.cs
+++ EL_2022_09_10_2D/Assets/Program/Scripts/SC_MG_Game.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class SC_MG_Game : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
